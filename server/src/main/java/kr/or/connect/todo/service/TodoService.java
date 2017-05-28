2fffea4efe76620c9c8fcf8c5793838367c7d81c
yk1028 <wongeunsong94@gmail.com>
2017-05-28 21:03:40 +0900

    할 일 등록하기 -service

--- server/src/main/java/kr/or/connect/todo/service/TodoService.java
+++ server/src/main/java/kr/or/connect/todo/service/TodoService.java
@@ -1,5 +1,19 @@
 package kr.or.connect.todo.service;
 
-public class TodoService {
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
 
+import kr.or.connect.domain.Todo;
+import kr.or.connect.todo.persistence.TodoDao;
+
+@Service
+@ComponentScan(basePackages = "kr.or.connect.persistence")
+public class TodoService {
+	private TodoDao dao;
+	
+	public Todo create(Todo todo) {
+		Integer id = dao.insert(todo);
+		todo.setId(id);
+		return todo;
+	}
 }
