2da34f402a3ca2c3ea437fb938543ac03daf48b0
yk1028 <wongeunsong94@gmail.com>
2017-05-28 20:54:23 +0900

    할 일 등록하기 -js틀 생

--- fe/js/app.js
+++ fe/js/app.js
@@ -1,8 +1,24 @@
 (function (window) {
 	'use strict';
+    var list = $(".todo-list");
 
 	// Your starting point. Enjoy the ride!
 
+    //create new todo
+    $(".new-todo").on( "keydown", function( event ) {
+        var context = $(this).val();
+        if(event.which == 13 && context != ""){
+            $.ajax({
+                url: "/api/todos",
+                type: 'GET',
+                data: {"context" : context},
+                success: function(responce){
+
+                }
+
+            })
+        }
+    });
 
 })(window);
 
--- server/src/main/java/kr/or/connect/todo/persistence/TodoDao.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoDao.java
@@ -17,4 +17,6 @@ public class TodoDao {
 				.withTableName("todo")
 				.usingGeneratedKeyColumns("id");
 	}
+	
+	
 }
